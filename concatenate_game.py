764f43fcdbf476f23a0772b84329d98212956351
Sahana-26 <sahanaa2602@gmail.com>
2023-08-29 19:05:01 +0530

    Rock_paper_scissors

---
+++ concatenate_game.py
@@ -0,0 +1,20 @@
+print("this a game where you provide the words we ask and create a meaningful paragrapgh for you.")
+print("LET'S START THE GAME")
+
+print("this is about MySelf!!!!")
+name= input("name:")
+adjective1= input("adjective:")
+adjective2= input("adjective:")
+adjective3=input("adjective:")
+person = input("your fav person:")
+a = input("a_quality_of_person:")
+character = input("one word description abt tht person:")
+love_language = input("your language:")
+word = input("happy_word:")
+wish= input("wish:")
+
+print("HERE'S YOUR PARAGRAPH")
+print("this is about MySelf!!!!")
+
+madlib = f"I'm {name}. I would describe myself as a {adjective1},{adjective2} \n and {adjective3} person. I love to help myfriends out by giving them advice with their problems, and by having their back whenever they need it by supporting them in activities such as going to outing to being there emotionally. My favorite person is {person} ,she always keeps {a} me when iam low. I like her {character}. I would always love to {love_language} her. shes there for me forever and iam always {word} her. {wish}. THANK YOU!"
+print(madlib)
